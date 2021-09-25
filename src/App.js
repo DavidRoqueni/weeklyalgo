b2c0e24c711d20636cf52849d932a819a1acde63
david <davidtroqueni@gmail.com>
2021-09-24 22:47:06 -0700

    init set up with a little simple nonrepeater

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  array = ['a', 'a', 'c', 'd', 'c', 'e', 'e', 'g', 'g', 'f'];
+  map = {};
+  firstNonRepeat = function () {
+    for (var i = 0; i < this.array.length; i++) {
+      if (this.array[i] in this.map) this.map[this.array[i]]++;
+      else this.map[this.array[i]] = 1;
+    }
+    for (let j = 0; j < this.array.length; j++) {
+      if (this.map[this.array[j]] === 1) return this.array[j];
+    }
+  };
+  render() {
+    return (
+      <div className='container'>
+        <h1>Sample Input</h1>
+        <div className='row sample'>[ {this.array.toString()} ]</div>
+        <div className='row'>
+          {this.array.map((e, index) => (
+            <div key={index} className='box col'>
+              {e.toUpperCase()}
+            </div>
+          ))}
+        </div>
+        <div className='answer row'>{this.firstNonRepeat()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
